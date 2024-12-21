b52c87c379aa9c4910b6e6b6143d59606a4390a1
Abhi0000000000011 <abrwt88@gmail.com>
2024-12-21 18:45:45 +0530

    Add files via upload

---
+++ 2nd.js
@@ -0,0 +1,151 @@
+/*
+let a = 3;
+let b = 9;
+console.log(" a + b = ", a + b);
+console.log(" a - b = ", a - b);
+console.log(" a * b = ", a * b);
+console.log(" a % b = ", a % b);
+console.log(" a = ", a +5);
+console.log(" b = ", b % 4);
+console.log(" b ** a = ", b ** a); // hahahahahahahahahahahahaha 9^3;
+*/
+
+// hahaha unary operator ;;;;;;;
+/*
+let a = 8;
+let b = 6;
+a = a+1;    // or a++
+console.log("a=", a);
+
+let a = 8;
+a--; // value agli line zse change hogi
+console.log("a=", a);
+
+
+let a = 8;
+--a;      //value usi line mae change hogii
+console.log("a=", a);
+
+like ....
+
+
+let a = 5;
+console.log(" a++ = ", a++); // ans is 5 
+console.log(" a =", a);  // here ans is 6    
+but here see down 
+
+let a =5;
+console.log(" a = ", ++a); // ans is 6 
+
+let a =5;
+a **= 2;
+console.log(" a = ", a);
+
+// comaprision operator
+
+a = 4;
+b = 4; // if b = "4" which is a string it gave ans ture bcz inside is is a no equal to that no.
+console.log(" c = ", a == b); // true if values are diff ans is false 
+ a = 4;
+ b = 4;
+ console.log(" c = ", a !== b); 
+a = 3;
+b = 7;
+console.log(' c = ', a < b );
+// logical operator
+let a = 8 
+let b = 7;
+console.log(" c = ", a > 10 || b < 2 )
+let a = 8;
+console.log(" c = ", !(a>1) ); // its ans is true but bcz of ! its output is false
+let a = 9;
+console.log(" c = ", !( a > 10)); // its ans is false but output is ture bcz of 
+// conditional statement
+let age = 6;
+if (age > 18) 
+{
+console.log("can vote");
+} 
+if (age < 18)
+{
+    console.log("cannot vote");
+}
+
+@
+let mode = "lightmode";
+let color;
+if (mode === "darkmode")
+{
+    (color = "black");
+}
+if (mode === "lightmode")
+{
+    (color = "white");
+}
+console.log(color);
+@
+
+// check no. odd / even 
+let no = 9883431111112;
+if ( no % 2 == 0)
+{
+console.log("even");
+}
+else
+{
+    console.log("odd");
+}  
+// else if statement
+ let x = 98;
+ if ( x < 18)
+ {
+     console.log(" cannot vote");
+ }
+else if ( x > 60)
+{
+    console.log(" voting for quite a time ");
+}
+else
+{
+    console.log(" ready to vote");
+}            
+// ternary operator       
+percentage = 55;
+percentage > 33 ? "pass" : "fail"; // no ouitput as nothing to print   
+percentage = 5;
+result = (percentage > 33 ? "pass" : "fail");
+console.log(result); // now it will print  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
